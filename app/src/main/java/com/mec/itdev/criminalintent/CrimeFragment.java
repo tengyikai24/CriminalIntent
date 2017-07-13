9bd46cd87c6afaa516e45db346f14635edb5d186
alexmouse <tengyikai24@126.com>
2017-07-13 15:17:18 +0800

    s
    s

---
+++ app/src/main/java/com/mec/itdev/criminalintent/CrimeFragment.java
@@ -0,0 +1,29 @@
+package com.mec.itdev.criminalintent;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CrimeFragment extends Fragment {
+
+
+    public CrimeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_crime, container, false);
+    }
+
+}
